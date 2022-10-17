ff3d6c75ce273ec8a43d3394ef31bc323236b4ca
Tanya Romanova <heavens1xdoor@mail.ru>
2022-10-17 19:27:21 +0300

    feat: complete task extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let newStr = [];
+  let newStrInner = [];
+  let resultStr = '';
+  let resultInnerStr = '';
+  if (options.addition !== undefined) {
+    for (let j = 0; j < (options.additionRepeatTimes || 1); j++) {
+      newStrInner.push(options.addition + '');
+    }
+  }
+
+  if (options.additionSeparator !== undefined) {
+    resultInnerStr = newStrInner.join(options.additionSeparator)
+  } else {
+    resultInnerStr = newStrInner.join('|')
+  }
+
+  for (let i = 0; i < (options.repeatTimes || 1); i++) {
+    newStr.push(str + '' + resultInnerStr) 
+  }
+
+  if (options.separator !== undefined) {
+    resultStr = newStr.join(options.separator)
+  } else {
+    resultStr = newStr.join('+')
+  }
+
+ return resultStr;
 }
 
 module.exports = {
